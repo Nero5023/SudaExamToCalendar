07f0b134a64cc1b3d054e8898e4f4181b39d98c8
Nero Zuo <502350123@qq.com>
2017-06-15 08:05:40 +0800

    first commit

---
+++ loginxk.py
@@ -0,0 +1,126 @@
+# -*- coding:utf-8 -*-
+
+import requests
+import sys
+import re
+import urllib
+from lxml import etree
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+def getVerifyCode(img):
+    verifyCode = raw_input("Enter the verify code: ")
+    return verifyCode
+
+def parseExamHTML(examHTML):
+    root = etree.HTML(examHTML)
+    tableRowsXPATH = "//tr"
+    tableRows = root.xpath(tableRowsXPATH)
+    res = []
+    for index, row in enumerate(tableRows):
+        if index == 0:
+            continue
+        tdXPath = 'td'
+        tds = row.xpath(tdXPath)
+        tdtexts = map(lambda x: x.text, tds)
+        if tdtexts[3].isspace():
+            continue
+        # print ' '.join(tdtexts)
+        res.append(' '.join(tdtexts))
+    return '\n'.join(res)
+
+
+def loginXK():
+    header = {
+        "Host": "xk.suda.edu.cn",
+        "Connection": "keep - alive",
+        "Upgrade-Insecure-Requests": "1",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Accept-Language": "zh-CN,zh;q=0.8,en;q=0.6,zh-TW;q=0.4"
+    }
+    loginURL = "http://xk.suda.edu.cn/default_szdx.aspx"
+
+    checkCodeImgURL = "http://xk.suda.edu.cn/CheckCode.aspx"
+    rs = requests.session()
+    loginPageHTML = rs.get(url=loginURL, headers=header).content
+
+    vsDicRe = r'<input type="hidden" name="(.*?)" value="(.*?)"'
+    vsDic = dict(re.findall(vsDicRe, loginPageHTML))
+    viewState = vsDic["__VIEWSTATE"]
+
+    checkCodeImg = rs.get(checkCodeImgURL).content
+    with open("checkCode.jpg", 'wb') as f:
+        f.write(checkCodeImg)
+    verifyCode = getVerifyCode(checkCodeImg)
+
+    studentId = "1427407028"
+    pwd = "znh706."
+
+    postdata = {
+        "__VIEWSTATE": viewState,
+        "TextBox1": studentId,
+        "TextBox2": pwd,
+        "TextBox3": verifyCode,
+        "Button1": ""
+    }
+
+    loginContent = rs.post(url=loginURL, data=postdata).content
+    with open("result.html", 'w') as f:
+        f.write(loginContent)
+
+    loginResRe = r"<script language='javascript' defer>alert\('(.*?)'\)"
+
+    loginResult = re.findall(loginResRe, loginContent)
+    if len(loginResult) == 0:
+        print "Login Success"
+    else:
+        loginResult = loginResult[0].decode('gbk').encode('utf-8')
+        print loginResult
+        print "Please login again"
+        loginXK()
+
+    examTimeURLDic = {
+        "xh": studentId,
+        "xm": "%D7%F3%B3%BD%BA%C0",
+        "gnmkdm": "N121610"
+    }
+    examTimeURLPar = urllib.urlencode(examTimeURLDic)
+    examTimeURL = "http://xk.suda.edu.cn/xskscx.aspx?" + examTimeURLPar
+
+    header["Referer"] = "http://xk.suda.edu.cn/xs_main.aspx?xh=" + studentId
+    examTimeRes = rs.get(url=examTimeURL, headers=header).content
+    examTimeRes = examTimeRes.decode('gbk')
+    examTimeRes = examTimeRes.encode('utf-8')
+    with open("examRes.html", 'w') as f:
+        f.write(examTimeRes)
+
+    examTimeText = parseExamHTML(examTimeRes)
+
+    print examTimeText
+
+def loginSession():
+    header = {
+        "Host": "xk.suda.edu.cn",
+        "Connection": "keep - alive",
+        "Upgrade-Insecure-Requests": "1",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Accept-Language": "zh-CN,zh;q=0.8,en;q=0.6,zh-TW;q=0.4"
+    }
+    loginURL = "http://xk.suda.edu.cn/default_szdx.aspx"
+
+    checkCodeImgURL = "http://xk.suda.edu.cn/CheckCode.aspx"
+    rs = requests.session()
+    loginPageHTML = rs.get(url=loginURL, headers=header).content
+
+    vsDicRe = r'<input type="hidden" name="(.*?)" value="(.*?)"'
+    vsDic = dict(re.findall(vsDicRe, loginPageHTML))
+    viewState = vsDic["__VIEWSTATE"]
+
+    checkCodeImg = rs.get(checkCodeImgURL).content
+    return rs, viewState, checkCodeImg
+
+
+loginXK()
